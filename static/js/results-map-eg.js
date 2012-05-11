d534551bc524d06bc93566936ad9f1488da315da
Hatem Mahmoud <hatem@expressionlab.com>
2012-05-11 14:54:26 +0200

    Replaced the image of French candidates

--- static/js/results-map-eg.js
+++ static/js/results-map-eg.js
@@ -1291,7 +1291,7 @@ function nationalEnabled() {
 			border = '#C2C2C2';
 			photo = S(
 				'background:url(',
-					imgUrl( S( 'candidate-photos-fr-', params.year, '-', size, '.png' ) ),
+					imgUrl( S( 'candidate-photos-eg-', params.year, '-', size, '.png' ) ),
 				'); ',
 				'background-position:-',
 				election.candidates.by.id[candidate.id].index * size, 'px 0px; '
